5f0131ce248bcffaac7136934de648f8f980cbc5
WildboarG <959586@outlook.com>
2022-04-29 18:06:09 +0800

    emm

--- setup.py
+++ setup.py
@@ -15,4 +15,5 @@ setuptools.setup(
     packages=["City_Hero-sign"],
     install_requires=["requests","rich"],
     keywords=["sign", "City_Hero"],
-    python_requires=">=3.6",
+    python_requires=">=3.6"
+    )
