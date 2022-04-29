583c284e7e53a566d94317fd9bb67756cdf2e99b
WildboarG <959586@outlook.com>
2022-04-29 20:08:14 +0800

    ...

--- cityhero/main.py
+++ cityhero/main.py
@@ -1,6 +1,7 @@
-from City_Hero-sign import Hero
+from __init__ import Hero
 
-
-WechatId = "---------"
-PublicOpenID = "----------"
-Hero(WechatId=WechatId,PublicOpenID=PublicOpenID)
+zw = "osnijvy-3CV5q4cvE1nmrBANf1kY"
+zp = "oGwGAv79IBfaF6b8RU5x-teeSIuc"
+a = Hero(WechatId=zw, PublicOpenID=zp)
+b = a.seach
+print(str(b)+"hahah")
